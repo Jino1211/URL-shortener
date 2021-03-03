49206e6d6426424c6008218ab771c07c20db6f4b
Jino1211 <77382695+Jino1211@users.noreply.github.com>
2021-03-03 13:46:48 +0200

    added addurl function

--- api/shorturl.js
+++ api/shorturl.js
@@ -1,12 +1,22 @@
 const { json } = require("body-parser");
 const express = require("express");
 const router = express.Router();
+const { URL, DataBase } = require("../managedatabase");
+
 router.use(express.json());
 router.use(express.urlencoded());
 
+const DB = new DataBase();
+
 router.post("/", (req, res) => {
-  let { url } = req.body;
-  res.status(302).json({ msg: "origin Url: " + url });
+  DB.addURL(req.body.url);
+
+  res.status(200).json({
+    msg: `"origin Url:",
+      "short Url:"
+      "success"`,
+  });
+  // res.send("Url excised");
 });
 
 module.exports = router;
--- managedatabase.js
+++ managedatabase.js
@@ -1,31 +1,62 @@
-const fs = require("fs");
+const fsPromise = require("fs/promises");
+const { url } = require("inspector");
 const shortid = require("shortid");
 
 class DataBase {
-  constructor() {}
+  constructor() {
+    this.dataUrl = [];
+  }
 
-  checkIfUrlExists(URL) {
-    fs.readFile("./database/database.json", "utf8", (err, data) => {
-      if (err) {
-        return false;
+  //check if the current url is already exist in database, return false if not, and the original+shrink if yes.
+  checkIfUrlExists(url) {
+    // const data = getTheJsonBase();
+    for (let urlObj in data) {
+      if (url === urlObj[originUrl]) {
+        return urlObj;
       }
-    });
-  }
-  getUrl(url) {
-    if (!this.checkIfUrlExists(url)) {
-      return false;
     }
+    return false;
   }
 
-  update(updateData) {}
+  addURL(url) {
+    // if (this.checkIfUrlExists(url)) {
+    // return false;
+    // }
+    const newUrl = new URL(url);
+    this.dataUrl.push(newUrl);
+    console.log(this.dataUrl);
+    const data = JSON.stringify(this.dataUrl);
+    fsPromise
+      .writeFile("./database/database.json", data, "utf-8")
+      .then(() => {
+        console.log("Success :" + data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 class URL {
   constructor(URL) {
-    this.url = URL;
-  }
-
-  shrinkingUrl() {
+    this.originUrl = URL;
     this.shrinkUrl = shortid.generate();
+    this.createAt = new Date();
   }
 }
+
+// function getTheJsonBase() {
+//   fs.readFile("./database/database.json", "utf8").then((data) => {
+//     data = JSON.stringify(data);
+//     console.log(data);
+//   });
+//   if (err) {
+//     console.error("Cannot able to read the database:" + err);
+//   }
+//   try {
+//   } catch {
+//     console.error("Cannot able to parse the data:" + err);
+//   }
+// }
+
+module.exports = { DataBase, URL };
